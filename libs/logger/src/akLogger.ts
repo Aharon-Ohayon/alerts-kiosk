c74330be86f1b18af12262a27a03c6d5794ae613
Aharon Ohayon <aharon.ohayon@xmcyber.com>
2025-06-15 00:30:57 +0300

    Exposed inner logger for fastify

--- libs/logger/src/akLogger.ts
+++ libs/logger/src/akLogger.ts
@@ -2,25 +2,25 @@ import pino, { Logger } from 'pino';
 import { AkLoggerSettings } from './akLoggerSettings';
 
 export class AkLogger {
-  private readonly logger: Logger;
+    public readonly logger: Logger;
 
-  constructor(settings: AkLoggerSettings, public readonly sender: string) {
-    this.logger = pino({ name: sender, level: settings.minimumLevel });
-  }
+    constructor(settings: AkLoggerSettings, public readonly sender: string) {
+        this.logger = pino({ name: sender, level: settings.minimumLevel });
+    }
 
-  public debug(content: string, additionalInfo?: any): void {
-    this.logger.debug(additionalInfo, content);
-  }
+    public debug(content: string, additionalInfo?: any): void {
+        this.logger.debug(additionalInfo, content);
+    }
 
-  public info(content: string, additionalInfo?: any): void {
-    this.logger.info(additionalInfo, content);
-  }
+    public info(content: string, additionalInfo?: any): void {
+        this.logger.info(additionalInfo, content);
+    }
 
-  public warn(content: string, additionalInfo?: any): void {
-    this.logger.warn(additionalInfo, content);
-  }
+    public warn(content: string, additionalInfo?: any): void {
+        this.logger.warn(additionalInfo, content);
+    }
 
-  public error(content: string, additionalInfo?: any): void {
-    this.logger.error(additionalInfo, content);
-  }
+    public error(content: string, additionalInfo?: any): void {
+        this.logger.error(additionalInfo, content);
+    }
 }
