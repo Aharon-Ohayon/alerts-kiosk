10ee419ce4ca2f806c55d2469c8edd418cd267ab
Aharon Ohayon <aharon.ohayon@xmcyber.com>
2025-06-20 16:29:29 +0300

    Fixed card update

--- apps/kiosk-fe/src/app/alerts/alertCard.tsx
+++ apps/kiosk-fe/src/app/alerts/alertCard.tsx
@@ -1,6 +1,6 @@
 import { OrefAlertSeverity } from '@alerts-kiosk/oref-api';
 import { Alert, AlertProps } from 'antd';
-import { useMemo } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { differenceInMinutes } from 'date-fns';
 
 export interface AlertCardProps {
@@ -22,10 +22,15 @@ export const AlertCard = ({
     firstDetected,
     type
 }: AlertCardProps) => {
-    const minutesAgo = useMemo(
-        () => differenceInMinutes(new Date(), firstDetected),
-        [firstDetected]
-    );
+    const [minutesAgo, setMinutesAgo] = useState<number>(undefined);
+
+    const updateMinutes = () => {
+        setMinutesAgo(differenceInMinutes(new Date(), firstDetected));
+    };
+
+    useEffect(updateMinutes, [firstDetected]);
+
+    setInterval(updateMinutes, 3000);
 
     return (
         <Alert
